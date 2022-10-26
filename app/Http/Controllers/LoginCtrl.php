ff95a6ae22bbb0a70fb643e3f38c759c151e582b
20jz0114 <105410289+20jz0114@users.noreply.github.com>
2022-10-26 09:49:21 +0900

    LoginとLoginCtrlを変更

--- app/Http/Controllers/LoginCtrl.php
+++ app/Http/Controllers/LoginCtrl.php
@@ -1,19 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Login;
 use Illuminate\Http\Request;
 
 class LoginCtrl extends Controller
 {
+
+    
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
-        //
+        return Login::getPwdById($id);
     }
 
     /**
