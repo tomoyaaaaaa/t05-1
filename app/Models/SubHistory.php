5bdccd34f440eb9f5a484df9d8260f9c6a7806c0
unknown <20jz0122@jz.jec.ac.jp>
2022-11-07 13:51:49 +0900

    .
    .

--- app/Models/SubHistory.php
+++ app/Models/SubHistory.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class SubHistory extends Model
 {
     use HasFactory;
+    public static function getPwdById($id){
+ 
+        return $pwd = DB::select('Select pwd From LOGIN_VIEW where id='.$id);
+    }
 }
